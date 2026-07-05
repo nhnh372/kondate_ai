ef8f0b634ac4c40448949a4468c3e902604fb619
はやと <hayato@hayatonoMacBook-Pro.local>
2026-07-05 20:36:45 +0900

    Add Firestore test write

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -16,7 +18,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  await FirebaseFirestore.instance.collection('users').doc('testUser').set({
+    'name': 'Hayato',
+    'app': 'KONDATE AI',
+    'createdAt': FieldValue.serverTimestamp(),
+  });
   runApp(const KondateAI());
 }
 
